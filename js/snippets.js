b5c5244a4b99db5ea86382bb2cd90e6086593fcc
Anthony Bedford <tony.bedford@live.co.uk>
2018-04-21 07:30:47 +0100

    More fun and games.

--- js/snippets.js
+++ js/snippets.js
@@ -1,15 +1,20 @@
 // JavaScript snippets
 
+"use strict";
+
 var x = Math.floor(Math.random() * 10);     // returns a number between 0 and 9
 console.log(x)
 
 
-for (i=0; i<1000; i++) {
+for (var i=0; i<1000; i++) {
     x = Math.floor((Math.random()*100)+1); // between 1 and 100
     //console.log(x);
     if (x >= 100){
         console.log("You won the prize! %d", x)
     }
 }
+
+
+
 
 
